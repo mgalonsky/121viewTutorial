6cb677c9dd3f94037f76f3c1f17b5531abcc8482
Melissa Galonsky <mgalonsky@cs.hmc.edu>
2014-09-11 12:52:53 -0700

    Initial Commit

---
+++ viewTutorial/MGViewController.h
@@ -0,0 +1,13 @@
+//
+//  MGViewController.h
+//  viewTutorial
+//
+//  Created by Melissa Galonsky on 9/11/14.
+//  Copyright (c) 2014 Melissa Galonsky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MGViewController : UIViewController
+
+@end
---
+++ viewTutorial/MGViewController.m
@@ -0,0 +1,29 @@
+//
+//  MGViewController.m
+//  viewTutorial
+//
+//  Created by Melissa Galonsky on 9/11/14.
+//  Copyright (c) 2014 Melissa Galonsky. All rights reserved.
+//
+
+#import "MGViewController.h"
+
+@interface MGViewController ()
+
+@end
+
+@implementation MGViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
